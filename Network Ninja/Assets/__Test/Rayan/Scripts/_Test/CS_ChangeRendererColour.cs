03c38eeb548c9b27518b9f2eaa7a5e696ea7d894
Rayan Yousef <46996528+RayanYousef@users.noreply.github.com>
2023-04-24 16:09:50 +0200

    Mini Map Test

---
+++ Network Ninja/Assets/__Test/Rayan/Scripts/_Test/CS_ChangeRendererColour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CS_ChangeRendererColour : MonoBehaviour
+{
+
+
+    [SerializeField] Color Protected,MaxHealth,ModerateHealth,LowHealth;
+
+
+    public void ChangeColour()
+    {
+
+    }
+
+
+
+}
