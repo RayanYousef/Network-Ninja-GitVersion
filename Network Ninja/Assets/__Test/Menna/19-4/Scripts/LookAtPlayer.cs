c25353e2f4e57da821e356bdaa09b470a0608a83
Radwa Ahmed <48102202+RadwaAhmed4869@users.noreply.github.com>
2023-05-14 17:47:49 +0300

    HP canvas look at player camera

--- Network Ninja/Assets/__Test/Menna/19-4/Scripts/LookAtPlayer.cs
+++ Network Ninja/Assets/__Test/Menna/19-4/Scripts/LookAtPlayer.cs
@@ -7,16 +7,14 @@ public class LookAtPlayer : MonoBehaviour
     //make HP looak at player (camera)
     public Transform cam;
 
-    // Start is called before the first frame update
     void Start()
     {
-        cam = GameObjectsManager.Instance.Player.GetComponentInChildren<Camera>().transform;
+        //cam = GameObjectsManager.Instance.PlayerCamera.GetComponentInChildren<Camera>().transform;
+        
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         transform.LookAt(cam);
-        
     }
 }
