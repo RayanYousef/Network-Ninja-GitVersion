75a107a1aeb9093d45068266de5506035cb6d1ba
Radwa Ahmed <48102202+RadwaAhmed4869@users.noreply.github.com>
2023-05-14 14:03:39 +0300

    button clicked when press Enter

---
+++ Network Ninja/Assets/__Test/Radwa/Scripts/rTakeEnter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rTakeEnter : MonoBehaviour
+{
+    private Button myBtn;
+    void Start()
+    {
+        myBtn = GetComponent<Button>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            myBtn.onClick.Invoke();
+        }
+    }
+}
