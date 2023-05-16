dcfd69c879fbf560042ba5674153fca00dd51645
Radwa Ahmed <48102202+RadwaAhmed4869@users.noreply.github.com>
2023-05-16 19:39:56 +0300

    change walking allies animation

--- Network Ninja/Assets/__Test/Menna/For Hackathon/scripts/Formation/FormationAgent.cs
+++ Network Ninja/Assets/__Test/Menna/For Hackathon/scripts/Formation/FormationAgent.cs
@@ -12,34 +12,33 @@ public class FormationAgent : MonoBehaviour
 
     public int toFollowIndex;
     public Vector3 toFollow;
-    private bool changeIdle;
+
+    private bool hasAnimation = false;
 
     private void Start()
     {
-        //transform.localScale = Vector3.one; //0.05
-        anim = GetComponent<Animator>();
-        StartCoroutine(AgentMovement());
+        if (anim != null) { }
+        else anim = GetComponent<Animator>();
+
+        if (battalion != null) { }
+        else battalion = GetComponentInParent<Formation>();
+
+        toFollow = battalion.IndicatorsList[toFollowIndex];
+        agent.SetDestination(toFollow);
     }
 
-    IEnumerator AgentMovement()
+    private void FixedUpdate()
     {
-        if (toFollow != null)
+        if (agent.remainingDistance <= 0.1f && !agent.pathPending && !hasAnimation)
         {
-            while(Vector3.SqrMagnitude(this.transform.position - toFollow) <= 0.2)
-            {
-                toFollow = battalion.IndicatorsList[toFollowIndex];
-                agent.SetDestination(toFollow);
-                yield return null;
-            }
-            if (anim != null)
-            {
-                changeIdle = (UnityEngine.Random.Range(0, 2) == 1);
-                Debug.Log(changeIdle);
-                if (changeIdle)
-                    anim.SetBool("isFirstFightPose", true);
-                else
-                    anim.SetBool("isSecondFightPose", true);
-            }
+            bool changeIdle;
+            changeIdle = (UnityEngine.Random.Range(0, 2) == 1);
+            Debug.Log(changeIdle);
+            if (changeIdle)
+                anim.SetBool("isFirstFightPose", true);
+            else
+                anim.SetBool("isSecondFightPose", true);
+            hasAnimation = true;
         }
     }
 }
--- Network Ninja/Assets/__Test/Radwa/Scripts/rTempBossScript.cs
+++ Network Ninja/Assets/__Test/Radwa/Scripts/rTempBossScript.cs
@@ -6,19 +6,18 @@ public class rTempBossScript : MonoBehaviour
 {
     private void Start()
     {
-        Die();
-    }
-    void Die()
-    {
         StartCoroutine(WaitAndDie());
     }
 
     IEnumerator WaitAndDie()
     {
         yield return new WaitForSeconds(4.0f);
+        Die();
+    }
 
+    void Die()
+    {
         /// invoking this event will start winning 7arakat <3 *Menna's ideas*
         GetComponentInParent<EnemySpawner>().OnBigBossKilled?.Invoke();
     }
-
 }
