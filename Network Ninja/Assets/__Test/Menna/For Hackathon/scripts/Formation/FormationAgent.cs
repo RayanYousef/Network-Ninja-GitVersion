5520924b612aad9bbe15f4354a92120822c4b979
mennayehia <32792189+mennayehia@users.noreply.github.com>
2023-05-28 23:05:40 +0300

    ADD allies model

--- Network Ninja/Assets/__Test/Menna/For Hackathon/scripts/Formation/FormationAgent.cs
+++ Network Ninja/Assets/__Test/Menna/For Hackathon/scripts/Formation/FormationAgent.cs
@@ -36,7 +36,7 @@ public class FormationAgent : MonoBehaviour
         timer += Time.deltaTime;
 
         Debug.Log("d:" + agent.remainingDistance);
-        if (agent.remainingDistance <= 0.1f && !agent.pathPending && changeIdle == 0 && timer > 1)
+        if (agent.remainingDistance <= 0.05f && !agent.pathPending && changeIdle == 0 && timer > 1)
         {
             ChangeAnimation();
         }
