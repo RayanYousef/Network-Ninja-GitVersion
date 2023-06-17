000fd89535ad794cf933f9f624be64aaecdd6313
mennayehia <32792189+mennayehia@users.noreply.github.com>
2023-06-17 11:48:31 +0300

    make velocity = 0 onstate exit for enemies in chase state

--- Network Ninja/Assets/__Test/Menna/For Hackathon/scripts/state machine/_Chase.cs
+++ Network Ninja/Assets/__Test/Menna/For Hackathon/scripts/state machine/_Chase.cs
@@ -47,6 +47,8 @@ public class _Chase : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        agent.speed = 0;
+        agent.velocity = Vector3.zero;
     }
 
 }
