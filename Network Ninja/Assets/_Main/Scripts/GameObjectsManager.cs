98f082d30e8874e4075dd0e0158bbe3e0846faa4
Radwa Ahmed <48102202+RadwaAhmed4869@users.noreply.github.com>
2023-06-18 14:47:46 +0300

    GameObjectsManager

--- Network Ninja/Assets/_Main/Scripts/GameObjectsManager.cs
+++ Network Ninja/Assets/_Main/Scripts/GameObjectsManager.cs
@@ -20,12 +20,9 @@ public class GameObjectsManager : MonoBehaviour
     [SerializeField] GameObject spawnEffect;
 
     [Header("Boss")]
-    [SerializeField] m_CombatManager combatManager;
-    // [SerializeField] m_BossUI_Manager bossUiManager;
     [SerializeField] GameObject boss;
 
     [Header("Current Gate")]
-    //[SerializeField] PathController currentPathController;
     [SerializeField] GateController currentGate;
 
     public static GameObjectsManager Instance { get => instance; }
@@ -37,12 +34,9 @@ public class GameObjectsManager : MonoBehaviour
     public FormationAgent AllyPrefab { get => allyPrefab; }
     public GameObject SpawnEffect { get => spawnEffect; set => spawnEffect = value; }
     public Formation AllyBatalionPrefab { get => allyBatalionPrefab; set => allyBatalionPrefab = value; }
-   // public m_BossUI_Manager BossUiManager { get => bossUiManager; }
-    public m_CombatManager CombatManager { get => combatManager; }
     public CinemachineBrain CameraBrain { get => cameraBrain; set => cameraBrain = value; }
     public GateController CurrentGate { get => currentGate; set => currentGate = value; }
 
-    //public PathController CurrentPathController { get => currentPathController; set => currentPathController = value; }
 
     private void Awake()
     {
